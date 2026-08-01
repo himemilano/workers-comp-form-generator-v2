88513fe8f70062388fb051d2f280ec210ce61a92
himemilano <matsumatsuceo@gmail.com>
2026-08-01 10:12:18 +0900

    Update phone.ts

--- rules /phone.ts
+++ rules /phone.ts
@@ -1,1 +1,25 @@
+export function normalizePhoneNumber(
+  phoneNumber: string
+): string {
+  const cleaned = phoneNumber.replace(
+    /[^0-9]/g,
+    ""
+  );
+
+  if (cleaned.length < 10) {
+    throw new Error(
+      "電話番号が短すぎます"
+    );
+  }
+
+  return cleaned;
+}
+
+export function getPhoneDigits(
+  phoneNumber: string
+): string[] {
+  return normalizePhoneNumber(
+    phoneNumber
+  ).split("");
+}
 
